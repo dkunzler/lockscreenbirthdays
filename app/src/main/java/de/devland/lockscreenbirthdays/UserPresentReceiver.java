7e0c9e9e6904eed51eede2421c415f6b24a41466
dk <dk@davidkunzler.de>
2015-01-03 01:36:09 +0100

    clear all notifications when user is present

--- app/src/main/java/de/devland/lockscreenbirthdays/UserPresentReceiver.java
+++ app/src/main/java/de/devland/lockscreenbirthdays/UserPresentReceiver.java
@@ -1,5 +1,6 @@
 package de.devland.lockscreenbirthdays;
 
+import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -10,8 +11,7 @@ public class UserPresentReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        // TODO: This method is called when the BroadcastReceiver is receiving
-        // an Intent broadcast.
-//        throw new UnsupportedOperationException("Not yet implemented");
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.cancelAll();
     }
 }
