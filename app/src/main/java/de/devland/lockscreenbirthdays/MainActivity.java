cc94e001d02269ec66f23f38e9d37e5cff660b04
David Kunzler <dk@devland.de>
2019-01-07 23:05:52 +0100

    coloring of pages and correct redirection after onboarding

--- app/src/main/java/de/devland/lockscreenbirthdays/MainActivity.java
+++ app/src/main/java/de/devland/lockscreenbirthdays/MainActivity.java
@@ -1,36 +1,51 @@
 package de.devland.lockscreenbirthdays;
 
 import android.app.Activity;
-import android.app.FragmentManager;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
 import android.widget.Toolbar;
 
 import butterknife.BindView;
-import butterknife.ButterKnife;
 import de.devland.esperandro.Esperandro;
-import de.devland.lockscreenbirthdays.prefs.DefaultPrefs;
 import de.devland.lockscreenbirthdays.prefs.InternalPrefs;
 
 
 public class MainActivity extends Activity {
 
+    private static final int REQUEST_CODE_INTRO = 42;
+
     @BindView(R.id.toolbar)
     protected Toolbar toolbar;
+    private InternalPrefs internalPrefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        InternalPrefs internalPrefs = Esperandro.getPreferences(InternalPrefs.class, this);
-        Intent nextActivity;
+        internalPrefs = Esperandro.getPreferences(InternalPrefs.class, this);
         if (!internalPrefs.onBoardingFinished()) {
-            nextActivity = new Intent(this, OnBoardingActivity.class);
+            Intent nextActivity = new Intent(this, OnBoardingActivity.class);
+            startActivityForResult(nextActivity, REQUEST_CODE_INTRO);
         } else {
-            nextActivity = new Intent(this, BirthdayActivity.class);
+            Intent nextActivity = new Intent(this, BirthdayActivity.class);
+            startActivity(nextActivity);
+            finish();
         }
+    }
 
-        startActivity(nextActivity);
-        finish();
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE_INTRO) {
+            if (resultCode == RESULT_OK) {
+                // Finished the intro
+                internalPrefs.onBoardingFinished(true);
+                Intent nextActivity = new Intent(this, BirthdayActivity.class);
+                startActivity(nextActivity);
+                finish();
+            } else {
+                // Cancelled the intro
+                finish();
+            }
+        }
     }
 }
--- app/src/main/java/de/devland/lockscreenbirthdays/OnBoardingActivity.java
+++ app/src/main/java/de/devland/lockscreenbirthdays/OnBoardingActivity.java
@@ -11,10 +11,11 @@ import android.os.PowerManager;
 import android.provider.Settings;
 
 import com.heinrichreimersoftware.materialintro.app.IntroActivity;
-import com.heinrichreimersoftware.materialintro.app.NavigationPolicy;
 import com.heinrichreimersoftware.materialintro.slide.SimpleSlide;
 
 public class OnBoardingActivity extends IntroActivity {
+
+    private static final int POSITION_BATTERY = 2;
 
     private SimpleSlide batterySlide;
 
@@ -29,6 +30,8 @@ public class OnBoardingActivity extends IntroActivity {
                 .title("Welcome")
                 .description("This app shows the birthdays of your contacts as notification after turning on your device.")
                 .image(R.mipmap.ic_launcher_cupcake)
+                .background(R.color.md_blue_500)
+                .backgroundDark(R.color.primary_dark)
                 .build());
 
         addSlide(new SimpleSlide.Builder()
@@ -36,35 +39,38 @@ public class OnBoardingActivity extends IntroActivity {
                 .description("This app needs the permission to read contacts to function properly.")
                 .image(R.drawable.ic_contacts)
                 .permission(Manifest.permission.READ_CONTACTS)
+                .background(R.color.md_red_300)
+                .backgroundDark(R.color.primary_dark)
                 .build());
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !powerManager.isIgnoringBatteryOptimizations(getPackageName())) {
             batterySlide = new SimpleSlide.Builder()
                     .title("Battery Optimizations")
                     .description("Please turn off battery optimizations for this app to make sure it stays in the background and can remind you of your contact's birthdays.")
+                    .image(R.drawable.ic_battery)
                     .buttonCtaLabel("Turn off Battery Optimizations")
                     .buttonCtaClickListener(__ -> {
                         startActivity(new Intent(Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS, Uri.parse("package:" + getPackageName())));
                     })
+                    .background(R.color.md_green_700)
+                    .backgroundDark(R.color.primary_dark)
                     .canGoForward(false)
                     .build();
             addSlide(batterySlide);
-
-            setNavigationPolicy(new NavigationPolicy() {
-                @SuppressLint("NewApi")
-                @Override
-                public boolean canGoForward(int position) {
-                    if (position == 2) {
-                        return powerManager.isIgnoringBatteryOptimizations(getPackageName());
-                    }
-                    return true;
-                }
-
-                @Override
-                public boolean canGoBackward(int position) {
-                    return true;
-                }
-            });
         }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && getCurrentSlidePosition() == POSITION_BATTERY) {
+            PowerManager powerManager = (PowerManager) getSystemService(Context.POWER_SERVICE);
+            if (powerManager.isIgnoringBatteryOptimizations(getPackageName())) {
+                // we probably came from the dialog to whitelist our app and it worked -> Move on
+                setResult(RESULT_OK);
+                finish();
+            }
+        }
+
     }
 }
